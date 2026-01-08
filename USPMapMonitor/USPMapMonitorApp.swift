514a27888b938bc759d166051d1447d5b145ffa9
iOSdvlprK <110379993+iOSdvlprK@users.noreply.github.com>
2026-01-08 22:11:13 +0900

    Initial Commit

---
+++ USPMapMonitor/USPMapMonitorApp.swift
@@ -0,0 +1,17 @@
+//
+//  USPMapMonitorApp.swift
+//  USPMapMonitor
+//
+//  Created by joe on 1/8/26.
+//
+
+import SwiftUI
+
+@main
+struct USPMapMonitorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
